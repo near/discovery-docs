13859bcdbc7b6dc3b1bd8bdcd1c61317e9156504
gagdiez <gagdiez.c@gmail.com>
2023-03-02 14:09:07 +0100

    widget builds

--- src/components/social-widget.js
+++ src/components/social-widget.js
@@ -1,44 +1,52 @@
 import React from "react";
-import MonacoEditor from 'react-monaco-editor';
-import * as monaco_editor from 'monaco-editor';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export function WidgetEditor({ children, id=1 }) {
+export function WidgetEditor({ children, id = 1 }) {
 
   const uri = "https://near.social/#/embed/zavodil.near/widget/remote-code?name=Anna";
   const code = children.props.children.props.children;
 
-  return <>
-    <div class="monaco">
-      <MonacoEditor
-        height="250"
-        value={code}
-        options={{
-          minimap: { enabled: false },
-          wordWrap: 'on',
-          scrollBeyondLastLine: false,
-          fontSize: '14px',
-          renderLineHighlight: false,
-          hideMargin: true,
-          glyphMargin: false,
-          folding: false,
-          lineNumbers: false,
-          lineDecorationsWidth: 0,
-          lineNumbersMinChars: 0,
-          scrollBars:false,
-        }}
-        language={"javascript"}
-        onChange={(newValue, event) => {document.getElementById(`ifm${id}`).src = `${uri}&code=${newValue}`}}
-      />
-    </div>
-    <em> You can edit the code! </em>
+  return (
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        const MonacoEditor = require('react-monaco-editor').default;
 
-    <hr className="subsection" />
+        return <>
+          <div class="monaco">
+            <MonacoEditor
+              height="250"
+              value={code}
+              options={{
+                minimap: { enabled: false },
+                wordWrap: 'on',
+                scrollBeyondLastLine: false,
+                fontSize: '14px',
+                renderLineHighlight: false,
+                hideMargin: true,
+                glyphMargin: false,
+                folding: false,
+                lineNumbers: false,
+                lineDecorationsWidth: 0,
+                lineNumbersMinChars: 0,
+                scrollBars: false,
+              }}
+              language={"javascript"}
+              onChange={(newValue, event) => { document.getElementById(`ifm${id}`).src = `${uri}&code=${newValue}` }}
+            />
+          </div>
+          <em> You can edit the code! </em>
 
-    <h4>Resulting Widget</h4>
-    <div class="code_iframe">
-      <iframe id={`ifm${id}`} src={`${uri}&code=${code}`}></iframe>
-    </div>
-  </>
+          <hr className="subsection" />
+
+          <h4>Resulting Widget</h4>
+          <div class="code_iframe">
+            <iframe id={`ifm${id}`} src={`${uri}&code=${code}`}></iframe>
+          </div>
+        </>
+
+      }}
+    </BrowserOnly>
+  )
 }
 
 export default WidgetEditor;
